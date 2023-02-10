b1bb9ff786a7c9203ccbdd22c37787a44dcf47f2
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-02-10 11:32:00 -0800

    Yes

--- Top Down Car Game/Assets/Scripts/CoinScript.cs
+++ Top Down Car Game/Assets/Scripts/CoinScript.cs
@@ -5,8 +5,7 @@ using TMPro;
 
 public class CoinScript : MonoBehaviour
 {
-    [SerializeField] private int value = 0;
-    [SerializeField] TextMeshProUGUI coinText;
+    [SerializeField] private int _value = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +22,19 @@ public class CoinScript : MonoBehaviour
          if(other.gameObject.CompareTag("Player"))
         {
             Debug.Log("I've been hit!");
-            value +=1;
-            coinText.text = value.ToString ("0");
-        
-            //update the collectible count
+
+
+            if(this.gameObject.CompareTag("Coin"))
+            {
+            LevelManager.Instance.UpdateLevelCoinCount(_value);                
+            }
+
+            if(this.gameObject.CompareTag("Gas"))
+            {
+            LevelManager.Instance.UpdateGasAmount(_value);                
+            }
+
+            Destroy(this.gameObject);            
         }
     }
 }
--- Top Down Car Game/Assets/Scripts/LevelManager.cs
+++ Top Down Car Game/Assets/Scripts/LevelManager.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance;
 
     public GameObject PausePanel;
     public GameObject GameOverPanel;
+    public TextMeshProUGUI CoinCountText;
+    public TextMeshProUGUI GasCountText;
+    [SerializeField] private int _coinsCollected = 0;
+    [SerializeField] private int _gasAmount = 10;
     
     void Awake()
     {
@@ -18,6 +23,8 @@ public class LevelManager : MonoBehaviour
     void Start()
     {
       Time.timeScale = 1;
+      CoinCountText.text = _coinsCollected.ToString();
+      GasCountText.text = _gasAmount.ToString();
     }
 
     // Update is called once per frame
@@ -50,5 +57,15 @@ public class LevelManager : MonoBehaviour
     {
         Time.timeScale = 1;
         PausePanel.SetActive(false);
+    }
+    public void UpdateLevelCoinCount(int amount)
+    {
+        _coinsCollected += amount;
+        CoinCountText.text = _coinsCollected.ToString();
+    }
+    public void UpdateGasAmount(int amount)
+    {
+        _gasAmount += amount;
+        GasCountText.text = _gasAmount.ToString();
     }
 }

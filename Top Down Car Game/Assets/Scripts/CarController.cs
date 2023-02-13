e6c0e82010f910f3e093c426c11d03ea30ea2e6b
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-02-13 11:30:57 -0800

    Eed
    Fix Gas

--- Top Down Car Game/Assets/Scripts/CarController.cs
+++ Top Down Car Game/Assets/Scripts/CarController.cs
@@ -50,7 +50,7 @@ public class CarController : MonoBehaviour
      }
     private void OnTriggerEnter2D(Collider2D other) 
      {
-        if(other.gameObject,CompareTag("Start"))
+        if(other.gameObject.CompareTag("Start"))
         {
             //Yes
         }

30cd13b93f8e3727567a413d40cf031605edeaa5
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-03-07 11:01:04 -0800

    Added UI Manager

---
+++ Top Down Car Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    [SerializeField] private int _coinCount = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //If this is here twice then destroy me, else, don't
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Top Down Car Game/Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void RaceButtonPressed()
+    {
+        SceneManager.LoadScene("Game Mode");
+    }
+    public void SingleRacePressed()
+    {
+        SceneManager.LoadScene("Single Race");
+    }
+    public void CupRacePressed()
+    {
+        SceneManager.LoadScene("Course 1");
+    }
+}
